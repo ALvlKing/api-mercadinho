cd7ed0b3d9fcd4eda3765ed983d33e941e4454a0
ALvzin <kingalvl80@gmail.com>
2025-10-07 01:04:55 -0300

    clienteService.ts

--- src/service/clienteService.ts
+++ src/service/clienteService.ts
@@ -1,25 +1,26 @@
 import { prisma } from '../database/prisma';
-import { Medico } from '../generated/prisma';
+import { Cliente } from '../generated/prisma';
 
-type MedicoCreateData = Omit<Medico, 'id' | 'createdAt' | 'updatedAt'>;
-type MedicoUpdateData = Partial<MedicoCreateData>;
+type ClienteCreateData = Omit<Cliente, 'id' | 'createdAt' | 'updatedAt'>;
+type ClienteUpdateData = Partial<MedicoCreateData>;
 
-export const create = async (data: MedicoCreateData): Promise<Medico> => {
+export const create = async (data: ClienteCreateData): Promise<Cliente> => {
   return prisma.medico.create({ data });
 };
 
-export const getAll = async (): Promise<Medico[]> => {
-  return prisma.medico.findMany();
+export const getAll = async (): Promise<Cliente[]> => {
+  return prisma.cliente.findMany();
 };
 
-export const getById = async (id: number): Promise<Medico | null> => {
+export const getById = async (id: number): Promise<Cliente | null> => {
   return prisma.medico.findUnique({ where: { id } });
 };
 
-export const update = async (id: number, data: MedicoUpdateData): Promise<Medico> => {
+export const update = async (id: number, data: ClienteUpdateData): Promise<Cliente> => {
   return prisma.medico.update({ where: { id }, data });
 };
 
-export const remove = async (id: number): Promise<Medico> => {
-  return prisma.medico.delete({ where: { id } });
+export const remove = async (id: number): Promise<Cliente> => {
+  return prisma.cliente.delete({ where: { id } });
 };
+
